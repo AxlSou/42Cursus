0042fe6d01554627587388e2fa423d8c24f3c3a9
AxlSou <axlsoubielle@gmail.com>
2023-11-27 22:40:57 +0100

    New prototypes in libft

--- Libft/libft.h
+++ Libft/libft.h
@@ -14,15 +14,20 @@
 # define LIBFT_H
 # include <stdio.h>
 
-size_t	ft_strlen(const char *str);
-char *ft_strchr(const char *str, int c);
-int	ft_strncmp(const char *str1, const char *str2, size_t n);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memset(void *str, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+char *ft_strchr(const char *str, int c);
+int	ft_strncmp(const char *str1, const char *str2, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+int	ft_memcmp(const void *str1, const void *str2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
