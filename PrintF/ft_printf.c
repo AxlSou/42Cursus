11e6bf8dc43996cc1667aacc33f668a3056b2e8c
Axel Soubielle Almada <96891813+AxlSou@users.noreply.github.com>
2023-12-07 16:45:27 +0100

    Add files via upload

--- PrintF/ft_printf.c
+++ PrintF/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:32:02 by asoubiel          #+#    #+#             */
-/*   Updated: 2023/12/07 13:24:16 by asoubiel         ###   ########.fr       */
+/*   Updated: 2023/12/07 16:35:04 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@ int	ft_printf(char const *str, ...)
 	t_flags	*flags;
 
 	count = 0;
+	flags = malloc(sizeof(t_flags));
+	if (!flags)
+		return (0);
 	va_start(args, str);
 	while (*str)
 	{
 		if (*str == '%')
 		{
-			ft_check_flags(str, flags);
+			str++;
+			str = ft_check_flags(str, flags);
 		}
 		ft_putchar_fd(*str, 1);
 		str++;
@@ -35,5 +39,5 @@ int	ft_printf(char const *str, ...)
 
 int main(void)
 {
-	printf("%- d", 10);
+	printf("Test %+4.d termina", 10);
 }
--- PrintF/ft_printf_utils.c
+++ PrintF/ft_printf_utils.c
@@ -6,24 +6,45 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 12:06:21 by asoubiel          #+#    #+#             */
-/*   Updated: 2023/12/07 13:24:03 by asoubiel         ###   ########.fr       */
+/*   Updated: 2023/12/07 16:38:35 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void	ft_check_flags(char const *str, t_flags *flags)
+static int	ft_isinset(char const *set, char c)
 {
-	if (*str + 1 == '-')
-		flags->ljust = 1;
-	else if (*str + 1 == '0')
-		ft_zero_flag(str, flags); // To check
-	else if (*str + 1 == '.')
-		ft_precision_flag(str, flags); // To check
-	else if (*str + 1 == '#')
-		ft_hashtag_flag(str); // To check
-	else if (*str + 1 == ' ')
-		ft_putchar_fd(' ', 1);
-	else if (*str + 1 == '+')
-		flags->plus = 1;
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_check_flags(char const *str, t_flags *flags)
+{
+	while (!ft_isinset("cspdiuxX%", *str))
+	{
+		if (*str == '-')
+			flags->ljust = 1;
+/* 		else if (*str == '0')
+			ft_zero_flag(str, flags); // To check
+		else if (*str == '.')
+			ft_precision_flag(str, flags); // To check */
+		else if (*str == '#')
+			flags->hasht = 1;
+		else if (*str == ' ')
+			ft_putchar_fd(' ', 1);
+		else if (*str == '+')
+			flags->plus = 1;
+		else if (*str == '%')
+			ft_putchar_fd('%', 1);
+		str++;
+	}
+	return ((char *)str);
 }
