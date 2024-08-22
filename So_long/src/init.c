e4c9add90fbeef45047db0bd424462c3a124db4b
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-08-22 20:39:30 +0200

    Add a few functions to open a window with game->floor

--- So_long/src/init.c
+++ So_long/src/init.c
@@ -6,17 +6,47 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 20:26:08 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/08/17 21:23:08 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/08/22 20:10:51 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void init(t_game *game)
+void	init(t_game *game)
 {
+	int			x;
+	int			y;
+	mlx_image_t	*img1;
+	mlx_image_t	*img2;
+
 	game->mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
 	if (!game->mlx)
 		error();
-
 	load_img(game);
+	img1 = mlx_texture_to_image(game->mlx, game->floor1);
+	if (!img1)
+		error();
+	img2 = mlx_texture_to_image(game->mlx, game->floor2);
+	if (!img2)
+		error();
+	x = 0;
+	while (x < WIDTH)
+	{
+		y = 0;
+		while (y < HEIGHT)
+		{
+			if ((x + y) % 32 == 0)
+			{
+				if (mlx_image_to_window(game->mlx, img1, x, y) < 0)
+					error();
+			}
+			else
+			{
+				if (mlx_image_to_window(game->mlx, img2, x, y) < 0)
+					error();
+			}
+			y += 16;
+		}
+		x += 16;
+	}
 }
--- So_long/src/main.c
+++ So_long/src/main.c
@@ -6,23 +6,31 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:13:50 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/08/17 21:04:36 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/08/22 20:06:23 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_game *game;
+	t_game	*game;
+	mlx_t	*mlx;
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <map>\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-	else
-	{
-		init(&game);
-	}
+	game = (t_game *)malloc(sizeof(t_game));
+	mlx = NULL;
+	(void)argc;
+	(void)argv;
+	// if (argc != 2)
+	// {
+	// 	printf("Usage: %s <map>\n", argv[0]);
+	// 	return (EXIT_FAILURE);
+	// }
+	// else
+	// {
+	// 	init(game);
+	// }
+	init(game);
+	mlx_loop(game->mlx);
+	return (EXIT_SUCCESS);
 }
