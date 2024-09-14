b93835e8f5050345c979906df5deca1397e85ce9
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-09-14 14:01:48 +0200

    Add new load_player and load_floor images functions

--- So_long/src/load_img.c
+++ So_long/src/load_img.c
@@ -6,18 +6,45 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:21:43 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/08/22 20:04:58 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:37:09 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void load_img(t_game *game)
+static void load_player(t_game *game)
 {
-	game->floor1 = mlx_load_png("./assets/Grass1.png");
+	mlx_texture_t	*player;
+
+	player = mlx_load_png("./assets/player/Player.png");
+	if (!player)
+		error();
+	game->player_img->player_d = mlx_texture_to_image(game->mlx, player);
+	if (!game->player_img->player_d)
+		error();
+}
+
+static void	load_img_floor(t_game *game)
+{
+	mlx_texture_t	*floor1;
+	mlx_texture_t	*floor2;
+
+	floor1 = mlx_load_png("./assets/floor/Grass1.png");
+	if (!floor1)
+		error();
+	floor2 = mlx_load_png("./assets/floor/Grass2.png");
+	if (!floor2)
+		error();
+	game->floor1 = mlx_texture_to_image(game->mlx, floor1);
 	if (!game->floor1)
 		error();
-	game->floor2 = mlx_load_png("./assets/Grass2.png");
+	game->floor2 = mlx_texture_to_image(game->mlx, floor2);
 	if (!game->floor2)
 		error();
+}
+
+void	load_img(t_game *game)
+{
+	load_img_floor(game);
+	load_player(game);
 }
