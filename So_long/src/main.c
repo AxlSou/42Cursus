0974ef51e4f08b01c0c6a2748acb1789e0c46af2
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-09-14 14:03:11 +0200

    Add malloc for player_img structure

--- So_long/src/main.c
+++ So_long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:13:50 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/08/22 20:06:23 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:39:20 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,19 @@
 int	main(int argc, char **argv)
 {
 	t_game	*game;
-	mlx_t	*mlx;
 
 	game = (t_game *)malloc(sizeof(t_game));
-	mlx = NULL;
-	(void)argc;
-	(void)argv;
-	// if (argc != 2)
-	// {
-	// 	printf("Usage: %s <map>\n", argv[0]);
-	// 	return (EXIT_FAILURE);
-	// }
-	// else
-	// {
-	// 	init(game);
-	// }
-	init(game);
-	mlx_loop(game->mlx);
+	game->player_img = (t_player_img *)malloc(sizeof(t_player_img));
+	if (argc != 2)
+	{
+		printf("Usage: %s <map>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		init(game);
+		mlx_loop(game->mlx);
+		mlx_terminate(game->mlx);
+	}
 	return (EXIT_SUCCESS);
 }
