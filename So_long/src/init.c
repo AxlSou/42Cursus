d81b854ea641ecc96c038ac5ae58e4c3f9f27142
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-09-14 22:19:56 +0200

    Add render ground and render walls function

--- So_long/src/init.c
+++ So_long/src/init.c
@@ -6,7 +6,7 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 20:26:08 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/09/14 13:02:09 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/09/14 22:15:53 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,40 +31,74 @@ static void	my_keyhook(mlx_key_data_t keydata, void *param)
 		move_player_right(game);
 }
 
-void	init(t_game *game)
+static void	render_ground(t_game *game)
 {
-	int			x;
-	int			y;
+	int	x;
+	int	y;
 
-	game->mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
-	if (!game->mlx)
-		error();
-	load_img(game);
-	x = 0;
-	while (x < WIDTH)
+	y = 0;
+	while (game->map[y])
 	{
-		y = 0;
-		while (y < HEIGHT)
+		x = 0;
+		while (game->map[y][x] && game->map[y][x] != '\n')
 		{
-			if ((x + y) % 32 == 0)
+			if (((x * 64) + (y * 64)) % 128 == 0)
 			{
-				if (mlx_image_to_window(game->mlx, game->floor1, x, y) < 0)
+				if (mlx_image_to_window(game->mlx, game->floor1,
+						x * 64, y * 64) < 0)
 					error();
 			}
 			else
 			{
-				if (mlx_image_to_window(game->mlx, game->floor2, x, y) < 0)
+				if (mlx_image_to_window(game->mlx, game->floor2,
+						x * 64, y * 64) < 0)
 					error();
 			}
-			y += 16;
+			x++;
 		}
-		x += 16;
+		y++;
 	}
+}
+
+static void	render_walls(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == '1')
+			{
+				if (mlx_image_to_window(game->mlx, game->wall,
+						x * 64, y * 64) < 0)
+					error();
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void	init(t_game *game)
+{
+	game->mlx = mlx_init(game->map_width * 64,
+			game->map_height * 64, "Test", true);
+	if (!game->mlx)
+		error();
+	load_img(game);
 	game->player_img->player_u = NULL;
 	game->player_img->player_r = NULL;
 	game->player_img->player_l = NULL;
 	game->player_pos[0] = 0;
 	game->player_pos[1] = 0;
+	game->collectibles = 0;
+	game->moves = 0;
+	render_ground(game);
+	render_walls(game);
 	if (mlx_image_to_window(game->mlx, game->player_img->player_d, 0, 0) < 0)
 		error();
 	mlx_key_hook(game->mlx, &my_keyhook, game);
