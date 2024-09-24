962e1cbd9f67421cad20368db77bec8306ff21d0
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-09-24 21:27:30 +0200

    Add free_map function

--- So_long/src/exit.c
+++ So_long/src/exit.c
@@ -6,18 +6,34 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 15:20:56 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/09/15 15:32:40 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/09/24 21:17:23 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+static void	free_map(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (game->map[i])
+	{
+		free(game->map[i]);
+		i++;
+	}
+	free(game->map);
+}
+
 void	exit_game(t_game *game)
 {
 	remove_player_img(game);
 	collect_lst_clear(game, &game->collectibles);
-	free(game->player_img);
+	if (game->player_img)
+		free(game->player_img);
 	mlx_terminate(game->mlx);
-	free(game);
+	free_map(game);
+	if (game)
+		free(game);
 	exit(0);
 }
