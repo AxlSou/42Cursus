a616f6fc52fcfd22df53989f1ef74fe64f66738b
Axel Soubielle Almada <axlsoubielle@gmail.com>
2024-09-30 20:27:52 +0200

    Delete texture after converting to Img

--- So_long/src/load_img.c
+++ So_long/src/load_img.c
@@ -6,7 +6,7 @@
 /*   By: asoubiel <asoubiel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:21:43 by asoubiel          #+#    #+#             */
-/*   Updated: 2024/09/15 17:03:54 by asoubiel         ###   ########.fr       */
+/*   Updated: 2024/09/30 20:04:35 by asoubiel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static void	load_player(t_game *game)
 	if (!player)
 		error_mlx();
 	game->player_img->player_d = mlx_texture_to_image(game->mlx, player);
+	mlx_delete_texture(player);
 	if (!game->player_img->player_d)
 		error_mlx();
 }
@@ -39,6 +40,8 @@ static void	load_img_floor(t_game *game)
 	if (!game->floor1)
 		error_mlx();
 	game->floor2 = mlx_texture_to_image(game->mlx, floor2);
+	mlx_delete_texture(floor1);
+	mlx_delete_texture(floor2);
 	if (!game->floor2)
 		error_mlx();
 }
@@ -51,6 +54,7 @@ static void	load_wall(t_game *game)
 	if (!wall)
 		error_mlx();
 	game->wall = mlx_texture_to_image(game->mlx, wall);
+	mlx_delete_texture(wall);
 	if (!game->wall)
 		error_mlx();
 }
@@ -64,6 +68,7 @@ void	load_collectible(t_game *game, t_collectibles *collectibles)
 		error_mlx();
 	collectibles->collectible_img = mlx_texture_to_image(game->mlx,
 			collect);
+	mlx_delete_texture(collect);
 	if (!collectibles->collectible_img)
 		error_mlx();
 }
@@ -79,6 +84,7 @@ void	load_img(t_game *game)
 	if (!exit)
 		error_mlx();
 	game->exit = mlx_texture_to_image(game->mlx, exit);
+	mlx_delete_texture(exit);
 	if (!game->exit)
 		error_mlx();
 }
